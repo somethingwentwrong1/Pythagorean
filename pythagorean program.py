cb9328af01116add21d7cbd67c000a440fc724f6
[Jonny Choi] <jonnychoi@seoulchristian.org>
2015-11-19 17:08:20 +0900

    It is a program for Pythagorean Theorem.

--- pythagorean program.py
+++ pythagorean program.py
@@ -1,1 +1,10 @@
-xxx
+my_height = 3
+my_hypotneuse = 5
+my_leg = 4
+
+
+print "If x is %s." % my_height
+print "If y is %d."%my_hypotneuse
+print "Then z is %d." % my_leg
+
+print "Is it right?" , (my_leg**2 + my_height**2 is my_hypotneuse**2)
