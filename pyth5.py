720123a8bedc0ad03f3c53f76252e006dc22ef10
[Jonny Choi] <jonnychoi@seoulchristian.org>
2016-01-24 19:14:33 +0900

    Fix pyth5.py

--- pyth5.py
+++ pyth5.py
@@ -8,7 +8,8 @@ while running:
     print("1 = Find Hypotenuse")
     print("2 = Find Base or Height")
     print("3 = Quit")
-# The equation for finding Hypotenuse
+# Insert the numbers of the user to my equation setting.
+# Finding Hypotenuse is square root Base**2 + Height**2
     cmd = int(input("Enter number : "))
     if cmd == 1:
         print("Find Hypotenuse")
@@ -16,14 +17,15 @@ while running:
         second = int(input("Enter Height :"))
         result = (first**2 + second**2)**(0.5)
         print(first, '+', second, '=', result)
-# The equation for finding Base or Height.
+# Insert the numbers of the user to my equation setting.
+# Finding Base or Height is square roo Hypotenuse**2 - Base or Height**2
     elif cmd == 2:
         print("Find Base or Height")
         first = int(input("Enter Hypotenuse :"))
         second = int(input("Enter the other Base or Height you know:"))
         result = (first**2 - second**2)**(0.5)
         print(first, '-', second, '=', result)
-# Quit.
+# Quit if the user selects 3.
     elif cmd == 3:
         print 'Good Bye:)'
         sys.exit(0)
